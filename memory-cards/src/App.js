04c1084acbcf333f75ad661d818017009d0e4a05
jakebrowning90 <jacobbrowning90@gmail.com>
2023-04-06 13:50:40 -0400

    Create boilerplate page and test layout

---
+++ memory-cards/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import './styles/reset.css';
+import './styles/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="pageHeader">Header</div>
+      <div id="pageBody">
+        <div id="cardField">
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+          <div className="card"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
