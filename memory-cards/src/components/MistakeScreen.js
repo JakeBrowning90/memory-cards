63827c36c169e583934e7cbed0fce789564e8bed
jakebrowning90 <jacobbrowning90@gmail.com>
2023-04-17 14:35:52 -0400

    Change text on popups

--- memory-cards/src/components/MistakeScreen.js
+++ memory-cards/src/components/MistakeScreen.js
@@ -5,13 +5,13 @@ const MistakeScreen = (props) => {
 
     return (
         <div id="mistakeScreen" className="popUp">
-            <h1 className="popHeader">Whoops!</h1>
+            <h1 className="popHeader">Too bad!</h1>
             <div className="popBody">
                 <h2>You clicked </h2>
                 <img src={lastCard}/> 
-                <h2> twice!</h2>
+                <h2> twice.</h2>
             </div>
-            <button id="restartButton" className="popFooter" onClick={onClick}>Try again</button>
+            <button id="restartButton" className="popFooter" onClick={onClick}>Try again?</button>
         </div>
     );
 };
